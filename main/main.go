7bb3e6cbd85b6da00bbfe3185289702727aa30b5
ben <benjamin.kusen@syntio.net>
2021-03-03 16:30:13 +0100

    comment err

--- main/main.go
+++ main/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println("Wow, look at this!")
-	err := errors.New("please help me")
+	//err := errors.New("please help me")
 }
